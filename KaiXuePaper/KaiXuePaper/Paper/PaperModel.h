b19485e859e6e4f38fb80178a386e386bceab916
zhouluyao <zly77153@offcn.com>
2020-06-22 18:15:56 +0800

    配置podspec

---
+++ KaiXuePaper/KaiXuePaper/Paper/PaperModel.h
@@ -0,0 +1,17 @@
+//
+//  PaperModel.h
+//  KaiXuePaper
+//
+//  Created by zhouluyao on 6/22/20.
+//  Copyright © 2020 zhouluyao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PaperModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KaiXuePaper/KaiXuePaper/Paper/PaperModel.m
@@ -0,0 +1,13 @@
+//
+//  PaperModel.m
+//  KaiXuePaper
+//
+//  Created by zhouluyao on 6/22/20.
+//  Copyright © 2020 zhouluyao. All rights reserved.
+//
+
+#import "PaperModel.h"
+
+@implementation PaperModel
+
+@end
